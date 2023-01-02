946c49e83f2f3a4c3549887c25edeeece80414b6
YEZI <art11010@naver.com>
2023-01-02 19:39:52 +0900

    header

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// components
+import Header from './components/Header';
 
+function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <div className="body_wrap container"></div>
     </div>
   );
 }
---
+++ Front-end/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as H from '../styled/Header.style';
+
+function Header() {
+  return (
+    <div className="bg-sky-200">
+      <H.Title>Header</H.Title>
+    </div>
+  );
+}
+
+export default Header;
