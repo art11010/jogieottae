72a0f11ffb96702b2dee258d7054c3d7da34d5e2
YEZI <art11010@naver.com>
2023-01-06 16:35:34 +0900

    reset

--- src/components/Atom.jsx
+++
@@ -1,7 +0,0 @@
-import Button from './Atom/Button';
-import Input from './Atom/Input';
-import Radio from './Atom/Radio';
-import Title from './Atom/Title';
-import TitleSub from './Atom/TitleSub';
-
-export { Button, Input, Radio, Title, TitleSub };
