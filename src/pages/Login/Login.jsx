9e8c629c992470dc0437e09d9f428b41a3d62058
mj <minjooo9696@naver.com>
2023-01-09 22:02:54 +0900

    로그인 수정

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as L from './Login.style';
 
 import { Button, Input, ShadowBox, Title, TitleSub } from '../../components/Atom';
@@ -16,6 +17,10 @@ function Login() {
           <L.multipleLogin>OR</L.multipleLogin>
           <Button addclass="btn-block mt-3 btn-accent" text="카카오로 로그인"></Button>
         </ShadowBox>
+        <div className="w-1/4 m-auto flex items-center justify-center mt-5">
+          <Link to="/findPW" className="hover:text-main">비밀번호 찾기</Link>
+          <Link to="/join" className="hover:text-main">회원가입</Link>
+        </div>
       </div>
     </>
   );
