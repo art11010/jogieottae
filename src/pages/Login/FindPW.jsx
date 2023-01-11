293f38b03f296d03be69681df9c8345eea79dfca
mj <minjooo9696@naver.com>
2023-01-12 00:02:27 +0900

    add mypage menu

--- src/pages/Login/FindPW.jsx
+++ src/pages/Login/FindPW.jsx
@@ -4,7 +4,7 @@ import * as L from './Login.style';
 
 import { Button, Input, ShadowBox, TitleSub } from '../../components/Atom';
 
-function Join() {
+function FindPW() {
   return (
     <ShadowBox addclass="w-500 m-auto mt-40 mb-40">
       <TitleSub addclass="text-center mb-5">비밀번호 찾기</TitleSub>
@@ -16,4 +16,4 @@ function Join() {
   );
 }
 
-export default Join;
+export default FindPW;
