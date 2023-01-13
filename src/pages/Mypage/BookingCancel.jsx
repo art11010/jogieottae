f03d3f5dac655635248bd82c71082025c732ccb5
YEZI <art11010@naver.com>
2023-01-13 22:07:05 +0900

    add BookingCancel

--- src/pages/Mypage/BookingCancel.jsx
+++ src/pages/Mypage/BookingCancel.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Title, TitleSub, BorderBox } from '../../components/Atom';
+import {
+  Title,
+  TitleSub,
+  BorderBox,
+  ShadowBox,
+  Button,
+} from '../../components/Atom';
 
 import roomImg from '../../assets/img_room.jpeg';
 
-function BookingDetails() {
+function BookingCancel() {
   return (
     <>
-      <Title>상세보기</Title>
+      <Title>예약 취소 요청</Title>
       <BorderBox addClass="mt-5">
         <TitleSub>주문일자 : 2022.12.25 (일)</TitleSub>
         <p className="text-gray-400">예약번호 : Y01010101010101</p>
         <div className="mt-10 pt-10 flex border-t border-gray-400">
+          <label className="block mr-3">
+            <input
+              type="checkbox"
+              checked="checked"
+              className="checkbox checkbox-primary"
+            />
+          </label>
           <div>
             <TitleSub addClass="mb-5">상품 및 이용정보</TitleSub>
             <button className="text-left">
@@ -52,13 +64,47 @@ function BookingDetails() {
           </dl>
         </div>
       </BorderBox>
+      <ShadowBox addClass="mt-12">
+        <div className="flex">
+          <div>
+            <TitleSub addClass="mb-5">
+              취소 사유 <span className="text-main">*</span>
+            </TitleSub>
+            <select className="select select-bordered w-60 border-gray-400 focus:outline-none">
+              <option disabled selected>
+                선택해주세요
+              </option>
+              <option>단순 변심</option>
+              <option>예약정보 변경 (상품, 일정 등)</option>
+              <option>중복예약 / 이용조건 불만</option>
+              <option>타사 최저가 상품 이용</option>
+              <option>업소 요청에 따른 이용불가</option>
+            </select>
+          </div>
+          <dl className="ml-10 pl-10 border-l border-gray-400 flex-grow text-lg">
+            <dt className="mb-5">
+              <TitleSub>취소 및 환불 예상 정보</TitleSub>
+            </dt>
+            <dd>
+              <strong className="px-5">취소 상품 금액</strong> 80000원
+            </dd>
+            <dd>
+              <strong className="px-5">할인 금액</strong> -5000원
+            </dd>
+            <dd>
+              <strong className="px-5">예상 취소 수수료</strong> 0원
+            </dd>
+            <dd className="mt-5 pt-5 border-dashed border-gray-400 border-t">
+              <strong className="px-5">환불 예상 금액</strong> 75000원
+            </dd>
+          </dl>
+        </div>
+      </ShadowBox>
       <div className="mt-5 text-right">
-        <Link to="/mypage/booking/cancel" className="btn btn-secondary px-10">
-          예약 취소 요청
-        </Link>
+        <Button addClass="px-10">예약 취소 요청</Button>
       </div>
     </>
   );
 }
 
-export default BookingDetails;
+export default BookingCancel;
