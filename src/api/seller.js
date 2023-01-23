163b4289de7a846c540dbcc5b2ef1c5d12213a23
YEZI <art11010@naver.com>
2023-01-24 01:39:38 +0900

    Axios 수정~

--- src/api/seller.js
+++ src/api/seller.js
@@ -1,9 +1,9 @@
-import { customAxios } from './customAxios';
+import { axiosCustom } from './axiosCustom';
 
 export const getSellerProductList = async ({ queryKey }) => {
   const [_, mainPath, sellerId] = queryKey;
   try {
-    const res = await customAxios.get(
+    const res = await axiosCustom.get(
       `/seller/${mainPath}?sellerId=${sellerId}&page=0`
     );
     const data = res.data;
@@ -15,7 +15,7 @@ export const getSellerProductList = async ({ queryKey }) => {
 export const getSellerProductDetail = async ({ queryKey }) => {
   const [_, mainPath, id] = queryKey;
   try {
-    const res = await customAxios.get(
+    const res = await axiosCustom.get(
       `/seller/${mainPath}/detail?${mainPath}Id=${id}`
     );
     const data = res.data;
