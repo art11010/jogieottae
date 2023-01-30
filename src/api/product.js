5ab8e11dd57a2cfd92c912ba2abdb26f118af11d
mj <minjooo9696@naver.com>
2023-01-30 19:41:10 +0900

    [mj] 상품 상세

--- src/api/product.js
+++ src/api/product.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 // import { axiosCustom } from './axiosCustom';
 
-export const getProductView = async () => {
+export const getProductView = async ({ queryKey }) => {
   const [_, mainPath, sellerId] = queryKey;
   try {
     const res = await axios.get(
-      `/customer/${mainPath}/detail?productId=1`
-      // `/data/productView.json`
+      // `/customer/${mainPath}/detail?productId=1`
+      `/data/productView.json`
     );
     const data = res.data;
     return data;
